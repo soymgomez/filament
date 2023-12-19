00b1cadf43121c9b1338c56ba0031e09fbb6103e
Martín Gómez <6207345+soymgomez@users.noreply.github.com>
2023-12-19 13:21:12 +0100

    Update LogoutController.php
    Check multiple logins

--- packages/panels/src/Http/Controllers/Auth/LogoutController.php
+++ packages/panels/src/Http/Controllers/Auth/LogoutController.php
@@ -11,6 +11,16 @@ class LogoutController
     {
         Filament::auth()->logout();
 
+        $count = collect(session()->all())->filter(function ($value, $key) {
+            return strpos($key, 'login_') === 0;
+        })->count();
+
+        ($count == 1)
+            ? session()->invalidate()
+            : session()->migrate();
+
+        session()->regenerateToken();
+        
         return app(LogoutResponse::class);
     }
 }
